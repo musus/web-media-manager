1ec5b3ae17a867b785d32d08fe2a953c22f6c1f6
Susumu Seino <seino@susu.mu>
2019-12-05 16:02:45 +0900

    some fix

--- inc/wp-user-icon.php
+++ inc/wp-user-icon.php
@@ -5,10 +5,11 @@ add_action( 'edit_user_profile', 'wp_user_icon_fields' );
 
 function media_uploader_enqueue() {
 	wp_enqueue_media();
-	wp_register_script('media-uploader', plugins_url('media-uploader.js' , __FILE__ ), array('jquery'));
-	wp_enqueue_script('media-uploader');
+	wp_register_script( 'media-uploader', plugins_url( 'media-uploader.js', __FILE__ ), array( 'jquery' ) );
+	wp_enqueue_script( 'media-uploader' );
 }
-add_action('admin_enqueue_scripts', 'media_uploader_enqueue');
+
+add_action( 'admin_enqueue_scripts', 'media_uploader_enqueue' );
 
 
 function wp_user_icon_fields( $user ) { ?>
@@ -26,17 +27,17 @@ function wp_user_icon_fields( $user ) { ?>
 			<td>
 				<?php
 				// If is current user's profile (profile.php)
-				if ( defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE ) {
+				if ( defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE ) {
 					$user_id = get_current_user_id();
 					// If is another user's profile page
-				} elseif (! empty($_GET['user_id']) && is_numeric($_GET['user_id']) ) {
+				} elseif ( ! empty( $_GET['user_id'] ) && is_numeric( $_GET['user_id'] ) ) {
 					$user_id = $_GET['user_id'];
 					// Otherwise something is wrong.
 				} else {
 					die( 'No user id defined.' );
 				}
 				$author_id = $user_id;
-				$avatar = get_avatar( $author_id );
+				$avatar    = get_avatar( $author_id );
 				echo $avatar;
 				?>
 			</td>
@@ -48,7 +49,7 @@ function wp_user_icon_fields( $user ) { ?>
 			<td>
 				<form action="">
 					<input type="text" name="wp_user_icon" id="upload_image" value="<?php echo esc_attr( get_the_author_meta( 'wp_user_icon', $user->ID ) ); ?>" class="regular-text"/>
-					<p class="description"><input type='button' class="button" value="画像選択" id="upload_btn"/>  <input type='button' id="wpui_clear" class="clear-button button" value="リセット"/></p>
+					<p class="description"><input type='button' class="button" value="画像選択" id="upload_btn"/> <input type='button' id="wpui_clear" class="clear-button button" value="リセット"/></p>
 					<p class="description">画像選択・リセットを押した後はプロフィールを更新してください</p>
 				</form>
 			</td>
@@ -70,17 +71,18 @@ function wp_user_icon_upload_js() { ?>
 
 	<script type="text/javascript">
 
-        jQuery(document).ready(function($){
+        jQuery(document).ready(function ($) {
             var mediaUploader;
-            $('#upload_btn').click(function(e) {
+            $('#upload_btn').click(function (e) {
                 e.preventDefault();
                 if (mediaUploader) {
                     mediaUploader.open();
                     return;
                 }
                 mediaUploader = wp.media.frames.file_frame = wp.media({
-                     multiple: false });
-                mediaUploader.on('select', function() {
+                    multiple: false
+                });
+                mediaUploader.on('select', function () {
                     var attachment = mediaUploader.state().get('selection').first().toJSON();
                     $('#upload_image').val(attachment.url);
                 });
@@ -88,7 +90,7 @@ function wp_user_icon_upload_js() { ?>
             });
         });
 
-        jQuery(function($){
+        jQuery(function ($) {
             $("#wpui_clear").click(function () {
                 $("#upload_image").val("");
             });
@@ -111,21 +113,21 @@ function wp_user_icon_save_fields( $user_id ) {
 }
 
 // Apply filter
-add_filter( 'get_avatar' , 'wp_user_icon_change_avatar' , 1 , 5 );
+add_filter( 'get_avatar', 'wp_user_icon_change_avatar', 1, 5 );
 
 function wp_user_icon_change_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
 	$user = false;
 
 	if ( is_numeric( $id_or_email ) ) {
 
-		$id = (int) $id_or_email;
-		$user = get_user_by( 'id' , $id );
+		$id   = (int) $id_or_email;
+		$user = get_user_by( 'id', $id );
 
 	} elseif ( is_object( $id_or_email ) ) {
 
 		if ( ! empty( $id_or_email->user_id ) ) {
-			$id = (int) $id_or_email->user_id;
-			$user = get_user_by( 'id' , $id );
+			$id   = (int) $id_or_email->user_id;
+			$user = get_user_by( 'id', $id );
 		}
 
 	} else {
@@ -138,7 +140,7 @@ function wp_user_icon_change_avatar( $avatar, $id_or_email, $size, $default, $al
 
 		if ( $user_icon_url ) {
 			$avatar = $user_icon_url;
-			$avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+			$avatar = '<img alt="' . $alt . '" src="' . $avatar . '" class="avatar avatar-' . $size . ' photo" height="' . $size . '" width="' . $size . '" />';
 
 		}
 
