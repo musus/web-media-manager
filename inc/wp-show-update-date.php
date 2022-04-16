95394283c2485f34577c4e583a176517a53d6a74
scaltinov <scaltinov@gmail.com>
2022-04-16 15:54:17 +0900

    更新日付を表示するオプションで、正しい日付が表示されない #1 引数とpost_idの取得方法を修正

--- inc/wp-show-update-date.php
+++ inc/wp-show-update-date.php
@@ -1,8 +1,8 @@
 <?php
 // replace update date
-function change_modified($post)
+function change_modified( $value, $format, $post ) {
 {
-    $post_id = get_the_ID($post);
+    $post_id = $post->ID;
     $publish_date = get_the_time('Ymd', $post_id);
     $update_date = get_the_modified_date('Ymd', $post_id);
 
